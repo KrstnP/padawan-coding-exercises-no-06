6c5164beffb02589d1c1ac931607825aa6450dc9
Tin <krstnpangan@gmail.com>
2018-08-01 20:20:00 +0800

    Added prompt for from range and to range

--- exercise_01.rb
+++ exercise_01.rb
@@ -1,4 +1,10 @@
 print "provide a multiplier number: "
 multiplier = gets.chomp.to_i
 
-puts "multiplier given is #{multiplier}" 
+puts "multiplier given is #{multiplier}"
+
+print "Enter range from: "
+from = gets.chomp.to_i
+
+print "Enter range to: "
+to = gets.chomp.to_i
